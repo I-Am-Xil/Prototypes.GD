961e361450f4398eb71bca8612470e2a3ad7a43c
Xil <logan_amp@outlook.com>
2024-10-29 18:53:31 -0600

    Fixed bullet input and spawn related bugs

--- bullet-hell/bullet_spawner.gd
+++ bullet-hell/bullet_spawner.gd
@@ -6,27 +6,22 @@ extends Node2D
 @onready var player_node:CharacterBody2D = get_parent()
 @onready var fire_rate_timer: Timer = $Timer
 
-const ARM_NUMBER: float = 2.0
+const ARM_NUMBER: float = 6.0
 const DISPERSION: float = 0.0
-#WARNING: un fire rate igual al physics process o mayor o igual al doble del mismo causa un bug en la generacion de balas
-#   FIRE_RATE = {60, 120, 121, ...}
-const FIRE_RATE: float = 59.0
+const FIRE_RATE: float = 1.0/60.0 #NOTE: Anything above the physics update frame rate makes no sense
 const BULLET_SPEED: int = 200
 const MULTISHOT: int = 1
 
-var bullet_dance: int = 1
+enum {VOID_FIRST_BULLET = 1}
 
-func _ready() -> void:
-    return
+var bullet_dance: int = 0
 
 
 func _physics_process(delta: float) -> void:
-
     if Input.is_action_pressed("Shoot"):
         bullet_dance += 1
         if fire_rate_timer.is_stopped():
-            fire_rate_timer.start(1.0/FIRE_RATE)
-            print(fire_rate_timer.time_left)
+            fire_rate_timer.start(FIRE_RATE)
 
     if Input.is_action_just_released("Shoot"):
         await fire_rate_timer.timeout
@@ -34,7 +29,8 @@ func _physics_process(delta: float) -> void:
         bullet_dance = 0
         return
 
-    if fire_rate_timer.time_left < fire_rate_timer.wait_time - delta or fire_rate_timer.time_left == fire_rate_timer.wait_time or fire_rate_timer.is_stopped():
+    #NOTE: Desperation statement a.k.a. fixes weird input handling related bugs
+    if fire_rate_timer.time_left < fire_rate_timer.wait_time - delta or fire_rate_timer.is_stopped() or bullet_dance == VOID_FIRST_BULLET:
         return
 
     var arm_counter: int = 0
@@ -43,6 +39,7 @@ func _physics_process(delta: float) -> void:
         #NOTE: This zone is literaly just like writing shaders
         #   Make sure to save the ones you like
         #   if pattern is inconsistent: Use the bullet_dance variable instead of Time.get_ticks_msec()
+        #   don't worry about the hardcoded constants that's just how pattern creation operations work
 
         # bullet.rotation = bullet_angle(player_node.rotation, ARM_NUMBER, arm_counter, 3*PI/4.0, DISPERSION, 5*Time.get_ticks_msec()/1000.0)
         # bullet.rotation = bullet_angle(0, ARM_NUMBER, arm_counter, 3*PI/4.0, DISPERSION, Time.get_ticks_msec()/1000.0)
@@ -52,22 +49,12 @@ func _physics_process(delta: float) -> void:
         bullet.position = global_position + 10*Vector2(remap(round((1.0+sin(i))/2.0), 0, 1, -4, 0), 4).rotated(bullet.rotation)
         bullet_dance += 1
 
-        """
-        NOTE: idk what it does but it makes a flower somehow with sides in range(1, 4)->
-        var sides: float = 4
-        bullet.position = global_position + Vector2(25, 0).rotated(bullet.rotation) + 25*Vector2(cos(floor(0.5 + i*PI/sides)), 0).rotated(bullet.rotation)
-        """
-
         bullet.bullet_speed = BULLET_SPEED
         arm_counter += 1
         if arm_counter >= ARM_NUMBER:
             arm_counter = 0
         player_node.add_sibling(bullet)
     return
-
-
-func _process(_delta: float) -> void:
-    pass
 
 
 func bullet_angle(initial_rotation: float, arm_number: float, arm_counter: int, arm_separation: float, inaccuracy: float, offset: float) -> float:
